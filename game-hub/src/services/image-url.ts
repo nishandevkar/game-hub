f60be936e470b9a21c8e8ff38ce6c754a5fbb95b
nickthorton <nishandevkarnd@gmail.com>
2024-07-20 15:19:54 +0800

    Adds a placeholder for games with no image
    - Adds a placeholder image to the assets
    - uses placeholder image for games that do not have an image
    when requested from the backend

--- game-hub/src/services/image-url.ts
+++ game-hub/src/services/image-url.ts
@@ -1,5 +1,6 @@
+import noImage from '../assets/no-image-placeholder-6f3882e0.webp'
 const getCroppedImageUrl = (url: string) => {
-    if (!url) return '';
+    if (!url) return noImage;
     let target = 'media/';
      const targetIndex = url.indexOf(target) + target.length;
      return url.slice(0, targetIndex) + 'crop/600/400/' + url.slice(targetIndex);
